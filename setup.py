9b425457ed9f6954c22e7ed57f99d3f911e89fe4
Mateusz Kapusta <mitra@Voyager-2.local>
2025-08-19 22:11:47 +0200

    Add support for UV + migrate to newer version of Numpy API

--- setup.py
+++ setup.py
@@ -1,23 +1,75 @@
-from distutils.core import setup, Extension
-import numpy as np
-import sys
+from setuptools.command.build_ext import build_ext
+from pathlib import Path
+import subprocess
+import os
+from setuptools import Extension, setup
 
-# COMPILATION: python setup.py build_ext --inplace
 
-python_version = sys.version_info[0]
+class BuildWithPreprocessing(build_ext):
+    def run(self):
+        # Run the preprocessing step first
+        self.run_preprocessing()
 
-if python_version == 3:
-    module = Extension(name='Finite', 
-                       sources=['source/python_3_wrapper.c','source/finite.c'], 
-                       include_dirs=[np.get_include()])
-elif python_version == 2:
-    module = Extension(name='Finite', 
-                       sources=['source/python_wrapper.c','source/finite.c'], 
-                       include_dirs=[np.get_include()])
-else:
-    exit()
-    
+        # Make sure numpy include dirs are added **after numpy is installed**
+        self.add_numpy_include()
+
+        # Continue with normal build_ext
+        super().run()
+
+    def add_numpy_include(self):
+        try:
+            import numpy as np
+        except ImportError:
+            raise RuntimeError(
+                "NumPy must be installed before building this extension."
+            )
+
+        for ext in self.extensions:
+            if np.get_include() not in ext.include_dirs:
+                ext.include_dirs.append(np.get_include())
+
+    def run_preprocessing(self):
+        root = Path(__file__).parent.absolute()
+
+        cc = os.environ.get("CC", "gcc")
+        cflags = os.environ.get("CFLAGS", "-O2").split()
+
+        compile_cmd = [
+            cc,
+            *cflags,
+            "-o",
+            str(root / "precalculate_table"),
+            str(root / "source/precalculate_table.c"),
+            str(root / "source/elliptic_integral.c"),
+        ]
+
+        print("Running preprocessing step...")
+
+        try:
+            subprocess.run(compile_cmd, check=True, cwd=root)
+            print("✓ Compilation successful")
+
+            subprocess.run([str(root / "precalculate_table")], check=True, cwd=root)
+            print("✓ Preprocessing completed")
+
+        except subprocess.CalledProcessError as e:
+            print(f"Error during preprocessing: {e}")
+            raise
+
+
+ext_modules = [
+    Extension(
+        name="Finite",
+        sources=["source/python_3_wrapper.c", "source/finite.c"],
+        include_dirs=[],
+    )
+]
+
 setup(
-    name = 'Finite', 
-    ext_modules = [module],
+    name="Finite",
+    ext_modules=ext_modules,
+    cmdclass={
+        "build_ext": BuildWithPreprocessing,
+    },
+    setup_requires=["numpy"],
 )
--- source/python_3_wrapper.c
+++ source/python_3_wrapper.c
@@ -1,19 +1,19 @@
-#include <Python.h>
-#include "numpy/arrayobject.h"
 #include "finite.h"
+#include <Python.h>
+#include <numpy/arrayobject.h>
 
-/* This module provides an interface for calculating microlensing 
+/* This module provides an interface for calculating microlensing
  * magnifications for extended sources using the method proposed by
  * Bozza et al. 2018.
- * 
- * See for extending Python using C: 
+ *
+ * See for extending Python using C:
  *      https://docs.scipy.org/doc/numpy/user/c-info.how-to-extend.html
  *      https://docs.python.org/2/extending/extending.html
  *      https://dfm.io/posts/python-c-extensions/
- * 
+ *
  * P. Mroz @ IPAC, 30 Apr 2019
- * 
- * Update 29 Nov 2019 (@ Caltech): 
+ *
+ * Update 29 Nov 2019 (@ Caltech):
  * - added compatibility with python3 (I changed the module definiton
  *   structure (static struct PyModuleDef Finite3) and the module's
  *   initialization function (PyMODINIT_FUNC PyInit_Finite3). See
@@ -29,169 +29,165 @@
 
 #define NPY_NO_DEPRECATED_API NPY_1_7_API_VERSION
 
-static char func_docstring1[] = 
+static char func_docstring1[] =
     "Extended source magnification for a uniform source.";
-    
-static char func_docstring2[] = 
-    "Extended source magnification for a limb-darkened source.";
-    
-static char func_docstring3[] = 
+
+static char func_docstring2[] =
     "Extended source magnification for a limb-darkened source.";
 
-static PyObject *
-ampl_wrapper (PyObject *self, PyObject *args)
-{
-    
-    /* This is Python wrapper for ampl(u,rho) */
-    
-    PyObject *func0_obj,*func1_obj; 
-    /* pointers to PyObject objects containing pre-calculated magnifications */
-    double *func0,*func1; /* pointers to arrays containing the data */
-    double u,rho,A;
-    
-    /* Parsing arguments */
-    if (!PyArg_ParseTuple(args,"ddOO",&u,&rho,&func0_obj,&func1_obj)) return NULL;
-    
-    /* Interpret the input objects as numpy arrays */
-    PyObject *func0_ = PyArray_FROM_OTF(func0_obj,NPY_DOUBLE,NPY_IN_ARRAY);
-    PyObject *func1_ = PyArray_FROM_OTF(func1_obj,NPY_DOUBLE,NPY_IN_ARRAY);
-    
-    /* If that didn't work, throw an exception */
-    if ( func0_ == NULL || func1_ == NULL ) {
-        Py_XDECREF(func0_);
-        Py_XDECREF(func1_);
-        return NULL;
-    }
+static char func_docstring3[] =
+    "Extended source magnification for a limb-darkened source.";
 
-    /* Get pointers to the data as C-types */
-    func0 = (double *) PyArray_DATA(func0_);
-    func1 = (double *) PyArray_DATA(func1_);
-        
-    /* Call the external C function to calculate the magnification */
-    A = ampl(u,rho,func0,func1);
-    
-    /* Cleaning-up */
-    Py_DECREF(func0_);
-    Py_DECREF(func1_);
-    
-    /* Return the calculated value */
-    return Py_BuildValue("d",A);
+static PyObject *ampl_wrapper(PyObject *self, PyObject *args) {
+
+  /* This is Python wrapper for ampl(u,rho) */
+
+  PyObject *func0_obj, *func1_obj;
+  /* pointers to PyObject objects containing pre-calculated magnifications */
+  double *func0, *func1; /* pointers to arrays containing the data */
+  double u, rho, A;
+
+  /* Parsing arguments */
+  if (!PyArg_ParseTuple(args, "ddOO", &u, &rho, &func0_obj, &func1_obj))
+    return NULL;
+
+  /* Interpret the input objects as numpy arrays */
+  PyObject *func0_ =
+      PyArray_FROM_OTF(func0_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+  PyObject *func1_ =
+      PyArray_FROM_OTF(func1_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+
+  /* If that didn't work, throw an exception */
+  if (func0_ == NULL || func1_ == NULL) {
+    Py_XDECREF(func0_);
+    Py_XDECREF(func1_);
+    return NULL;
+  }
+
+  /* Get pointers to the data as C-types */
+  func0 = (double *)PyArray_DATA((PyArrayObject *)func0_);
+  func1 = (double *)PyArray_DATA((PyArrayObject *)func1_);
+
+  /* Call the external C function to calculate the magnification */
+  A = ampl(u, rho, func0, func1);
+
+  /* Cleaning-up */
+  Py_DECREF(func0_);
+  Py_DECREF(func1_);
+
+  /* Return the calculated value */
+  return Py_BuildValue("d", A);
 }
 
-static PyObject *
-ampl_ld_wrapper (PyObject *self, PyObject *args)
-{
-    
-    /* This is Python wrapper for ampl_ld(u,rho,Gamma,Lambda) */
-    
-    PyObject *func0_obj,*func1_obj; 
-    /* pointers to PyObject objects containing pre-calculated magnifications */
-    double *func0,*func1; /* pointers to arrays containing the data */
-    double u,rho,Gamma,Lambda,A;
-    int n_rings;
-    
-    /* Parsing arguments */
-    if (!PyArg_ParseTuple(args,"ddddOOi",&u,&rho,&Gamma,&Lambda,
-        &func0_obj,&func1_obj,&n_rings)) return NULL;
+static PyObject *ampl_ld_wrapper(PyObject *self, PyObject *args) {
 
-    /* Interpret the input objects as numpy arrays */
-    PyObject *func0_ = PyArray_FROM_OTF(func0_obj,NPY_DOUBLE,NPY_IN_ARRAY);
-    PyObject *func1_ = PyArray_FROM_OTF(func1_obj,NPY_DOUBLE,NPY_IN_ARRAY);
-    
-    /* If that didn't work, throw an exception */
-    if ( func0_ == NULL || func1_ == NULL ) {
-        Py_XDECREF(func0_);
-        Py_XDECREF(func1_);
-        return NULL;
-    }
+  /* This is Python wrapper for ampl_ld(u,rho,Gamma,Lambda) */
 
-    /* Get pointers to the data as C-types */
-    func0 = (double *) PyArray_DATA(func0_);
-    func1 = (double *) PyArray_DATA(func1_);
-        
-    /* Call the external C function to calculate the magnification */
-    A = ampl_ld(u,rho,Gamma,Lambda,func0,func1,n_rings);
+  PyObject *func0_obj, *func1_obj;
+  /* pointers to PyObject objects containing pre-calculated magnifications */
+  double *func0, *func1; /* pointers to arrays containing the data */
+  double u, rho, Gamma, Lambda, A;
+  int n_rings;
 
-    /* Cleaning-up */
-    Py_DECREF(func0_);
-    Py_DECREF(func1_);
-    
-    /* Return the calculated value */
-    return Py_BuildValue("d",A);
+  /* Parsing arguments */
+  if (!PyArg_ParseTuple(args, "ddddOOi", &u, &rho, &Gamma, &Lambda, &func0_obj,
+                        &func1_obj, &n_rings))
+    return NULL;
+
+  /* Interpret the input objects as numpy arrays */
+  PyObject *func0_ =
+      PyArray_FROM_OTF(func0_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+  PyObject *func1_ =
+      PyArray_FROM_OTF(func1_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+
+  /* If that didn't work, throw an exception */
+  if (func0_ == NULL || func1_ == NULL) {
+    Py_XDECREF(func0_);
+    Py_XDECREF(func1_);
+    return NULL;
+  }
+
+  /* Get pointers to the data as C-types */
+  func0 = (double *)PyArray_DATA((PyArrayObject *)func0_);
+  func1 = (double *)PyArray_DATA((PyArrayObject *)func1_);
+
+  /* Call the external C function to calculate the magnification */
+  A = ampl_ld(u, rho, Gamma, Lambda, func0, func1, n_rings);
+
+  /* Cleaning-up */
+  Py_DECREF(func0_);
+  Py_DECREF(func1_);
+
+  /* Return the calculated value */
+  return Py_BuildValue("d", A);
 }
 
-static PyObject *
-ampl_ld_array_wrapper (PyObject *self, PyObject *args)
-{
-    
-    /* This is Python wrapper for ampl_ld(u,rho,Gamma,Lambda) */
-    
-    PyObject *func0_obj,*func1_obj,*u_obj,*A_obj; 
-    /* pointers to PyObject objects containing pre-calculated magnifications */
-    double *func0,*func1; /* pointers to arrays containing the data */
-    double *u,rho,Gamma,Lambda,*A;
-    int i,n_rings;
-    
-    /* Parsing arguments */
-    if (!PyArg_ParseTuple(args,"OdddOOi",&u_obj,&rho,&Gamma,&Lambda,
-        &func0_obj,&func1_obj,&n_rings)) return NULL;
+static PyObject *ampl_ld_array_wrapper(PyObject *self, PyObject *args) {
 
-    /* Interpret the input objects as numpy arrays */
-    PyObject *func0_ = PyArray_FROM_OTF(func0_obj,NPY_DOUBLE,NPY_IN_ARRAY);
-    PyObject *func1_ = PyArray_FROM_OTF(func1_obj,NPY_DOUBLE,NPY_IN_ARRAY);
-    PyObject *u_ = PyArray_FROM_OTF(u_obj,NPY_DOUBLE,NPY_IN_ARRAY);
-    
-    /* If that didn't work, throw an exception */
-    if ( func0_ == NULL || func1_ == NULL || u_ == NULL) {
-        Py_XDECREF(func0_);
-        Py_XDECREF(func1_);
-        Py_XDECREF(u_);
-        return NULL;
-    }
+  /* This is Python wrapper for ampl_ld(u,rho,Gamma,Lambda) */
 
-    /* Get pointers to the data as C-types */
-    func0 = (double *) PyArray_DATA(func0_);
-    func1 = (double *) PyArray_DATA(func1_);
-    u = (double *) PyArray_DATA(u_);
-    npy_intp *dims = PyArray_DIMS(u_); //number of elements in u array
-            
-    /* Call the external C function to calculate the magnification */
+  PyObject *func0_obj, *func1_obj, *u_obj, *A_obj;
+  /* pointers to PyObject objects containing pre-calculated magnifications */
+  double *func0, *func1; /* pointers to arrays containing the data */
+  double *u, rho, Gamma, Lambda, *A;
+  int i, n_rings;
 
-    A = (double *) PyMem_Malloc(dims[0]*sizeof(double));
-    for (i=0; i<dims[0]; i++) {
-        A[i] = ampl_ld(u[i],rho,Gamma,Lambda,func0,func1,n_rings);
-    }
-    A_obj = PyArray_SimpleNew(1,dims,NPY_DOUBLE);
-    memcpy(PyArray_DATA(A_obj),A,dims[0]*sizeof(double));
-    
-    /* Cleaning-up */
-    Py_DECREF(func0_);
-    Py_DECREF(func1_);
-    Py_DECREF(u_);
-    
-    PyMem_Free(A);
-    
-    /* Return the calculated value */
-    return Py_BuildValue("N",A_obj);
+  /* Parsing arguments */
+  if (!PyArg_ParseTuple(args, "OdddOOi", &u_obj, &rho, &Gamma, &Lambda,
+                        &func0_obj, &func1_obj, &n_rings))
+    return NULL;
+
+  /* Interpret the input objects as numpy arrays */
+  PyObject *func0_ =
+      PyArray_FROM_OTF(func0_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+  PyObject *func1_ =
+      PyArray_FROM_OTF(func1_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+  PyObject *u_ = PyArray_FROM_OTF(u_obj, NPY_DOUBLE, NPY_ARRAY_IN_ARRAY);
+
+  /* If that didn't work, throw an exception */
+  if (func0_ == NULL || func1_ == NULL || u_ == NULL) {
+    Py_XDECREF(func0_);
+    Py_XDECREF(func1_);
+    Py_XDECREF(u_);
+    return NULL;
+  }
+
+  /* Get pointers to the data as C-types */
+  func0 = (double *)PyArray_DATA((PyArrayObject *)func0_);
+  func1 = (double *)PyArray_DATA((PyArrayObject *)func1_);
+  u = (double *)PyArray_DATA((PyArrayObject *)u_);
+  npy_intp *dims = PyArray_DIMS((PyArrayObject *)u_);
+
+  /* Call the external C function to calculate the magnification */
+
+  A = (double *)PyMem_Malloc(dims[0] * sizeof(double));
+  for (i = 0; i < dims[0]; i++) {
+    A[i] = ampl_ld(u[i], rho, Gamma, Lambda, func0, func1, n_rings);
+  }
+  A_obj = PyArray_SimpleNew(1, dims, NPY_DOUBLE);
+  memcpy(PyArray_DATA((PyArrayObject *)A_obj), A, dims[0] * sizeof(double));
+  /* Cleaning-up */
+  Py_DECREF(func0_);
+  Py_DECREF(func1_);
+  Py_DECREF(u_);
+
+  PyMem_Free(A);
+
+  /* Return the calculated value */
+  return Py_BuildValue("N", A_obj);
 }
 
 static PyMethodDef FiniteMethods[] = {
-    {"ampl_ld_array", (PyCFunction) ampl_ld_array_wrapper, METH_VARARGS, func_docstring3},
-    {"ampl_ld", (PyCFunction) ampl_ld_wrapper, METH_VARARGS, func_docstring2},
-    {"ampl", (PyCFunction) ampl_wrapper, METH_VARARGS, func_docstring1},
-    {NULL, NULL, 0, NULL}
-};
+    {"ampl_ld_array", (PyCFunction)ampl_ld_array_wrapper, METH_VARARGS,
+     func_docstring3},
+    {"ampl_ld", (PyCFunction)ampl_ld_wrapper, METH_VARARGS, func_docstring2},
+    {"ampl", (PyCFunction)ampl_wrapper, METH_VARARGS, func_docstring1},
+    {NULL, NULL, 0, NULL}};
 
-static struct PyModuleDef Finite = {
-    PyModuleDef_HEAD_INIT,
-    "Finite",
-    "usage",
-    -1,
-    FiniteMethods
-};
+static struct PyModuleDef Finite = {PyModuleDef_HEAD_INIT, "Finite", "usage",
+                                    -1, FiniteMethods};
 
-PyMODINIT_FUNC PyInit_Finite(void)
-{
-    import_array();
-    return PyModule_Create(&Finite);
+PyMODINIT_FUNC PyInit_Finite(void) {
+  import_array();
+  return PyModule_Create(&Finite);
 }
